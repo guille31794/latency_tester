7355da6b9bdef748764906bf53f0a6aeaf272a13
Guillermo Girón <guille.g94@hotmail.es>
2022-08-25 18:57:26 +0200

    Añadido mapa de navegación del botón atrás y estructura inicial de configuración

--- LatencyTester/dataModel.hpp
+++ LatencyTester/dataModel.hpp
@@ -1,6 +1,10 @@
 #ifndef DATAMODEL_HPP
 #define DATAMODEL_HPP
 
+#include <QString>
+#include <QVector>
+#include <QMap>
+
 enum class MenuScreen
 {
     START_SCREEN,
@@ -12,5 +16,21 @@ enum class MenuScreen
     USER_MANUAL_SCREEN,
 
 };
+
+QMap<MenuScreen, MenuScreen> backButtonOutputs{{MenuScreen::HELP_SCREEN, MenuScreen::START_SCREEN},
+                                               {MenuScreen::USER_MANUAL_SCREEN, MenuScreen::START_MEASURE_SCREEN},
+                                               {MenuScreen::SETTINGS_SCREEN, MenuScreen::START_SCREEN},
+                                               {MenuScreen::GENERAL_INFO_SCREEN, MenuScreen::START_SCREEN},
+                                               {MenuScreen::START_MEASURE_SCREEN, MenuScreen::START_SCREEN},
+                                               {MenuScreen::MEASURES_REGISTRY_SCREEN, MenuScreen::START_SCREEN},
+                                              };
+
+struct GeneralConfig
+{
+    QString language;
+    bool daltonicMode;
+    quint8 fontSize;
+};
+
 
 #endif // DATAMODEL_HPP
