048d3a05bcc8d3491835db72571a70a44b90ffa5
Guillermo Giron <Guillermo.Giron@dtn.com>
2026-08-09 10:49:58 +0200

    Actualizado script de compilación arm64 con nuevas libs de enlazado.

--- LatencyTester/Scripts/Linux/build-arm64.sh
+++ LatencyTester/Scripts/Linux/build-arm64.sh
@@ -107,7 +107,9 @@ $QMAKE "$PROJECT_DIR/LatencyTester.pro" \
     "QMAKE_LINK=aarch64-linux-gnu-g++" \
     "QMAKE_CFLAGS+=--sysroot=$SYSROOT" \
     "QMAKE_CXXFLAGS+=--sysroot=$SYSROOT" \
-    "QMAKE_LFLAGS+=--sysroot=$SYSROOT -L$SYSROOT/usr/lib/aarch64-linux-gnu"
+    "QMAKE_LFLAGS+=--sysroot=$SYSROOT -L$SYSROOT/usr/lib/aarch64-linux-gnu -Wl,-rpath-link,$SYSROOT/usr/lib/aarch64-linux-gnu" \
+    "QMAKE_LIBDIR=$SYSROOT/usr/lib/aarch64-linux-gnu" \
+    "QMAKE_LIBDIR_QT=$SYSROOT/usr/lib/aarch64-linux-gnu"
 
 step "Compiling ($JOBS parallel jobs)"
 make -j"$JOBS"
